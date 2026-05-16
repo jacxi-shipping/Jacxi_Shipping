09c3bc883eb5f5e1ca644b2370f9911e9f1b1055
jacxi-shipping <info@jacxi.com>
2026-05-16 02:02:18 +0000

    errr

--- src/components/sections/RoutesAnimatedSection.tsx
+++ src/components/sections/RoutesAnimatedSection.tsx
@@ -156,7 +156,7 @@ export default function RoutesAnimatedSection() {
                   initial={{ opacity: 0 }}
                   animate={{
                     opacity: [0, 1, 1, 0],
-                    motionDistance: ["0%", "100%", "100%", "100%"]
+                    offsetDistance: ["0%", "100%", "100%", "100%"]
                   }}
                   transition={{
                     duration: p.duration * 2,
